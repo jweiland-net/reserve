53003fd7107d1b91d50e6ee7c3876c57e19bc696
Pascal Rinker <info@kronova.net>
2020-05-12 16:03:30 +0200

    [TASK] Add ext_emconf.php

---
+++ ext_emconf.php
@@ -0,0 +1,20 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Reserve',
+    'description' => '',
+    'category' => 'plugin',
+    'author' => 'Markus Kugler, Pascal Rinker',
+    'author_email' => 'projects@jweiland.net',
+    'author_company' => 'jweiland.net',
+    'state' => 'alpha',
+    'uploadfolder' => 0,
+    'clearCacheOnLoad' => 0,
+    'version' => '0.0.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-10.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
