1250ddebf32094743f7e24e446340319159d367d
crontis <dev@crontis.net>
2020-05-12 16:41:19 +0200

    [Task] Added entities
    Addresses #10 #1 #2 #3

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,24 +2,113 @@
 # Table structure for table 'tx_reserve_domain_model_facility'
 #
 CREATE TABLE tx_reserve_domain_model_facility (
-   uid int(11) NOT NULL auto_increment,
-   pid int(11) DEFAULT '0' NOT NULL,
-   tstamp int(11) DEFAULT '0' NOT NULL,
-   crdate int(11) DEFAULT '0' NOT NULL,
-   cruser_id int(11) DEFAULT '0' NOT NULL,
-   sys_language_uid int(11) DEFAULT '0' NOT NULL,
-   l10n_parent int(11) DEFAULT '0' NOT NULL,
-   l10n_diffsource mediumtext,
-   l10n_source int(11) DEFAULT '0' NOT NULL,
-   deleted tinyint(4) DEFAULT '0' NOT NULL,
-   hidden tinyint(4) DEFAULT '0' NOT NULL,
-   starttime int(11) DEFAULT '0' NOT NULL,
-   endtime int(11) DEFAULT '0' NOT NULL,
-   sorting int(11) DEFAULT '0' NOT NULL,
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+	tstamp int(11) DEFAULT '0' NOT NULL,
+	crdate int(11) DEFAULT '0' NOT NULL,
+	cruser_id int(11) DEFAULT '0' NOT NULL,
+	sys_language_uid int(11) DEFAULT '0' NOT NULL,
+	l10n_parent int(11) DEFAULT '0' NOT NULL,
+	l10n_diffsource mediumtext,
+	l10n_source int(11) DEFAULT '0' NOT NULL,
+	deleted tinyint(4) DEFAULT '0' NOT NULL,
+	hidden tinyint(4) DEFAULT '0' NOT NULL,
+	starttime int(11) DEFAULT '0' NOT NULL,
+	endtime int(11) DEFAULT '0' NOT NULL,
+	sorting int(11) DEFAULT '0' NOT NULL,
 
-   name tinytext,
+	name tinytext,
+	periods int(11) DEFAULT '0' NOT NULL,
 
-   PRIMARY KEY (uid),
-   KEY parent (pid),
-   KEY sys_language_uid_l10n_parent (sys_language_uid,l10n_parent)
+	PRIMARY KEY (uid),
+	KEY parent (pid),
+	KEY sys_language_uid_l10n_parent (sys_language_uid,l10n_parent)
+);
+
+#
+# Table structure for table 'tx_reserve_domain_model_period'
+#
+CREATE TABLE tx_reserve_domain_model_period (
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+	tstamp int(11) DEFAULT '0' NOT NULL,
+	crdate int(11) DEFAULT '0' NOT NULL,
+	cruser_id int(11) DEFAULT '0' NOT NULL,
+	sys_language_uid int(11) DEFAULT '0' NOT NULL,
+	l10n_parent int(11) DEFAULT '0' NOT NULL,
+	l10n_diffsource mediumtext,
+	l10n_source int(11) DEFAULT '0' NOT NULL,
+	deleted tinyint(4) DEFAULT '0' NOT NULL,
+	hidden tinyint(4) DEFAULT '0' NOT NULL,
+	starttime int(11) DEFAULT '0' NOT NULL,
+	endtime int(11) DEFAULT '0' NOT NULL,
+	sorting int(11) DEFAULT '0' NOT NULL,
+
+	begin int(11) DEFAULT '0' NOT NULL,
+	end int(11) DEFAULT '0' NOT NULL,
+	max_participants int(11) DEFAULT '0' NOT NULL,
+	max_participants_per_order int(11) DEFAULT '0' NOT NULL,
+	booking_begin int(11) DEFAULT '0' NOT NULL,
+	orders int(11) DEFAULT '0' NOT NULL,
+
+	PRIMARY KEY (uid),
+	KEY parent (pid),
+	KEY sys_language_uid_l10n_parent (sys_language_uid,l10n_parent)
+);
+
+#
+# Table structure for table 'tx_reserve_domain_model_order'
+#
+CREATE TABLE tx_reserve_domain_model_order (
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+	tstamp int(11) DEFAULT '0' NOT NULL,
+	crdate int(11) DEFAULT '0' NOT NULL,
+	cruser_id int(11) DEFAULT '0' NOT NULL,
+	sys_language_uid int(11) DEFAULT '0' NOT NULL,
+	l10n_parent int(11) DEFAULT '0' NOT NULL,
+	l10n_diffsource mediumtext,
+	l10n_source int(11) DEFAULT '0' NOT NULL,
+	deleted tinyint(4) DEFAULT '0' NOT NULL,
+	hidden tinyint(4) DEFAULT '0' NOT NULL,
+	starttime int(11) DEFAULT '0' NOT NULL,
+	endtime int(11) DEFAULT '0' NOT NULL,
+	sorting int(11) DEFAULT '0' NOT NULL,
+
+	booked_period int(11) DEFAULT '0' NOT NULL,
+	activation_code varchar(64) DEFAULT '0' NOT NULL,
+	email tinytext,
+	reservations int(11) DEFAULT '0' NOT NULL,
+
+	PRIMARY KEY (uid),
+	KEY parent (pid),
+	KEY sys_language_uid_l10n_parent (sys_language_uid,l10n_parent)
+);
+
+#
+# Table structure for table 'tx_reserve_domain_model_reservation'
+#
+CREATE TABLE tx_reserve_domain_model_reservation (
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+	tstamp int(11) DEFAULT '0' NOT NULL,
+	crdate int(11) DEFAULT '0' NOT NULL,
+	cruser_id int(11) DEFAULT '0' NOT NULL,
+	sys_language_uid int(11) DEFAULT '0' NOT NULL,
+	l10n_parent int(11) DEFAULT '0' NOT NULL,
+	l10n_diffsource mediumtext,
+	l10n_source int(11) DEFAULT '0' NOT NULL,
+	deleted tinyint(4) DEFAULT '0' NOT NULL,
+	hidden tinyint(4) DEFAULT '0' NOT NULL,
+	starttime int(11) DEFAULT '0' NOT NULL,
+	endtime int(11) DEFAULT '0' NOT NULL,
+	sorting int(11) DEFAULT '0' NOT NULL,
+
+	customer_order int(11) DEFAULT '0' NOT NULL,
+	code varchar(64) DEFAULT '0' NOT NULL,
+	used tinyint(4) DEFAULT '0' NOT NULL,
+
+	PRIMARY KEY (uid),
+	KEY parent (pid),
+	KEY sys_language_uid_l10n_parent (sys_language_uid,l10n_parent)
 );
