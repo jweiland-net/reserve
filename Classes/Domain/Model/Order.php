7e0387c654df4a2cd103e350ad14e44d0580f66e
Pascal Rinker <info@kronova.net>
2020-05-13 11:11:41 +0200

    [TASK] Add Facility and Period model

--- Classes/Domain/Model/Order.php
+++ Classes/Domain/Model/Order.php
@@ -25,16 +25,86 @@ use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
  */
 class Order extends AbstractEntity
 {
+    /**
+     * @var \JWeiland\Reserve\Domain\Model\Period
+     */
+    protected $bookedPeriod;
+
     protected $activationCode = '';
+
     protected $email = '';
 
     /**
-     * @var ObjectStorage<\JWeiland\Reserve\Domain\Model\Reservation>
+     * @var \TYPO3\CMS\Extbase\Persistence\ObjectStorage<\JWeiland\Reserve\Domain\Model\Reservation>
      */
     protected $reservations;
 
     public function __construct()
     {
         $this->reservations = new ObjectStorage();
+    }
+
+    /**
+     * @return Period
+     */
+    public function getBookedPeriod(): Period
+    {
+        return $this->bookedPeriod;
+    }
+
+    /**
+     * @param Period $bookedPeriod
+     */
+    public function setBookedPeriod(Period $bookedPeriod)
+    {
+        $this->bookedPeriod = $bookedPeriod;
+    }
+
+    /**
+     * @return string
+     */
+    public function getActivationCode(): string
+    {
+        return $this->activationCode;
+    }
+
+    /**
+     * @param string $activationCode
+     */
+    public function setActivationCode(string $activationCode)
+    {
+        $this->activationCode = $activationCode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return ObjectStorage
+     */
+    public function getReservations(): ObjectStorage
+    {
+        return $this->reservations;
+    }
+
+    /**
+     * @param ObjectStorage $reservations
+     */
+    public function setReservations(ObjectStorage $reservations)
+    {
+        $this->reservations = $reservations;
     }
 }
