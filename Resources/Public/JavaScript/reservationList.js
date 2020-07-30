ea46028645e2870a5a774718d2e04f13e835e530
Pascal Rinker <info@kronova.net>
2020-07-30 11:24:18 +0200

    [TASK] Check for reservation table or management table in JS before init

--- Resources/Public/JavaScript/reservationList.js
+++ Resources/Public/JavaScript/reservationList.js
@@ -1,7 +1,9 @@
-let reserveConfReservation = document.getElementById('reserve-conf');
-reservationConfig = {'datatables': {}};
-if (reserveConfReservation) {
-    reservationConfig = JSON.parse(reserveConfReservation.getAttribute('data-conf'));
-}
+if (document.getElementsByClassName('tx-reserve-periods').length) {
+    let reserveConfReservation = document.getElementById('reserve-conf');
+    reservationConfig = {'datatables': {}};
+    if (reserveConfReservation) {
+        reservationConfig = JSON.parse(reserveConfReservation.getAttribute('data-conf'));
+    }
 
-$('#datatable').DataTable(reservationConfig.datatables);
+    $('#datatable').DataTable(reservationConfig.datatables);
+}
