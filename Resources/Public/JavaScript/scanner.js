20a340a787872be82ff8a3c150d6e561e992ed16
Pascal Rinker <info@kronova.net>
2020-07-31 11:12:11 +0200

    [TASK] Update JS, Template

--- Resources/Public/JavaScript/scanner.js
+++ Resources/Public/JavaScript/scanner.js
@@ -35,13 +35,15 @@ if (document.getElementsByClassName('tx-reserve-management').length) {
         }
         activeScan = true;
 
+        let url = '';
+        if ($('#entireOrder').is(':checked')) {
+            url = $(this).siblings('a[data-scan="all"]')[0].href;
+        } else {
+            url = this.href;
+        }
+
         let request = $.ajax({
-            url: this.href,
-            data: {
-                tx_reserve_management: {
-                    entireOrder: $('#entireOrder').is(':checked') ? 1 : 0
-                }
-            }
+            url: url
         });
 
         request.done((response) => {
@@ -68,120 +70,120 @@ if (document.getElementsByClassName('tx-reserve-management').length) {
             );
         });
     });
-}
 
-function createModal(title, message, classes = '', $additionalElement = null)
-{
-    let $modal = $('<div class="modal">');
+    function createModal(title, message, classes = '', $additionalElement = null)
+    {
+        let $modal = $('<div class="modal">');
 
-    let $title = $('<h3>').text(title);
-    let $message = $('<p>').text(message);
-    let $close = $('<button>').css('float', 'right').width(200).height(40).text('close').on('click', () => {
-        $.modal.close();
-    });
+        let $title = $('<h3>').text(title);
+        let $message = $('<p>').text(message);
+        let $close = $('<button>').css('float', 'right').width(200).height(40).text('close').on('click', () => {
+            $.modal.close();
+        });
 
-    $modal.addClass(classes);
+        $modal.addClass(classes);
 
-    $modal.append($title).append($message).append($additionalElement).append($close);
+        $modal.append($title).append($message).append($additionalElement).append($close);
 
-    $modal.appendTo('body').modal();
-}
-
-function initializeScanner(video) {
-    if (!navigator.mediaDevices) {
-        return;
+        $modal.appendTo('body').modal();
     }
 
-    let canvas = canvasElement.getContext('2d');
-    let loadingMessage = document.getElementById('loadingMessage');
-
-    let startTime = 0;
-
-    function drawLine(begin, end, color) {
-        canvas.beginPath();
-        canvas.moveTo(begin.x, begin.y);
-        canvas.lineTo(end.x, end.y);
-        canvas.lineWidth = 4;
-        canvas.strokeStyle = color;
-        canvas.stroke();
-    }
-
-    navigator.mediaDevices.getUserMedia({
-        video: { facingMode: 'environment' },
-        width: screen.width
-    }).then(function(stream) {
-        video.srcObject = stream;
-        video.setAttribute('autoplay', true);
-        video.setAttribute('muted', true);
-        video.setAttribute('playsinline', true);
-        video.onloadedmetadata = function(e) {
-            video.play();
-        };
-        requestAnimationFrame(tick);
-    });
-
-    let qrReader = new Worker(`/typo3conf/ext/reserve/Resources/Public/JavaScript/qrReader.js`);
-    let qrReaderReady = true;
-
-    function tick(timestamp) {
-        if (!startTime) {
-            startTime = timestamp;
+    function initializeScanner(video) {
+        if (!navigator.mediaDevices) {
+            return;
         }
 
-        let delta = timestamp - startTime;
+        let canvas = canvasElement.getContext('2d');
+        let loadingMessage = document.getElementById('loadingMessage');
 
-        if (video.readyState === video.HAVE_ENOUGH_DATA) {
-            loadingMessage.hidden = true;
-            canvasElement.hidden = false;
+        let startTime = 0;
 
-            let scale = video.videoWidth / canvasElement.parentNode.parentElement.offsetWidth;
-            canvasElement.width = canvasElement.parentNode.parentElement.offsetWidth;
-            canvasElement.height = video.videoHeight / scale;
-            canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-            canvasElement.style.display = 'block';
-            canvasElement.style.margin = 'auto';
+        function drawLine(begin, end, color) {
+            canvas.beginPath();
+            canvas.moveTo(begin.x, begin.y);
+            canvas.lineTo(end.x, end.y);
+            canvas.lineWidth = 4;
+            canvas.strokeStyle = color;
+            canvas.stroke();
+        }
 
-            if (codeInImage) {
-                drawLine(codeInImage.location.topLeftCorner, codeInImage.location.topRightCorner, '#3BFF58');
-                drawLine(codeInImage.location.topRightCorner, codeInImage.location.bottomRightCorner, '#3BFF58');
-                drawLine(codeInImage.location.bottomRightCorner, codeInImage.location.bottomLeftCorner, '#3BFF58');
-                drawLine(codeInImage.location.bottomLeftCorner, codeInImage.location.topLeftCorner, '#3BFF58');
-            }
+        navigator.mediaDevices.getUserMedia({
+            video: { facingMode: 'environment' },
+            width: screen.width
+        }).then(function(stream) {
+            video.srcObject = stream;
+            video.setAttribute('autoplay', true);
+            video.setAttribute('muted', true);
+            video.setAttribute('playsinline', true);
+            video.onloadedmetadata = function(e) {
+                video.play();
+            };
+            requestAnimationFrame(tick);
+        });
 
-            if (delta > 300) {
+        let qrReader = new Worker(`/typo3conf/ext/reserve/Resources/Public/JavaScript/qrReader.js`);
+        let qrReaderReady = true;
+
+        function tick(timestamp) {
+            if (!startTime) {
                 startTime = timestamp;
-
-                if (qrReaderReady && !activeScan) {
-                    qrReaderReady = false;
-                    let imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
-                    qrReader.postMessage(imageData, [imageData.data.buffer]);
-                }
             }
-        } else {
-            loadingMessage.innerText = '⌛ ' + config.language.loading_video;
-        }
 
-        qrReader.onmessage = function(message) {
-            codeInImage = message.data;
+            let delta = timestamp - startTime;
 
-            if (codeInImage) {
-                reservations.search(codeInImage.data).draw();
+            if (video.readyState === video.HAVE_ENOUGH_DATA) {
+                loadingMessage.hidden = true;
+                canvasElement.hidden = false;
 
-                let $scan = $('tr[data-code="'+ codeInImage.data + '"]').find('a[data-action="scan"]');
+                let scale = video.videoWidth / canvasElement.parentNode.parentElement.offsetWidth;
+                canvasElement.width = canvasElement.parentNode.parentElement.offsetWidth;
+                canvasElement.height = video.videoHeight / scale;
+                canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+                canvasElement.style.display = 'block';
+                canvasElement.style.margin = 'auto';
 
-                if ($scan.length) {
-                    $scan.trigger('click');
-                } else {
-                    createModal(
-                        config.language.status.code_not_found.title,
-                        config.language.status.code_not_found.message,
-                        'error'
-                    );
+                if (codeInImage) {
+                    drawLine(codeInImage.location.topLeftCorner, codeInImage.location.topRightCorner, '#3BFF58');
+                    drawLine(codeInImage.location.topRightCorner, codeInImage.location.bottomRightCorner, '#3BFF58');
+                    drawLine(codeInImage.location.bottomRightCorner, codeInImage.location.bottomLeftCorner, '#3BFF58');
+                    drawLine(codeInImage.location.bottomLeftCorner, codeInImage.location.topLeftCorner, '#3BFF58');
                 }
-            }
-            qrReaderReady = true;
-        }
 
-        requestAnimationFrame(tick);
+                if (delta > 300) {
+                    startTime = timestamp;
+
+                    if (qrReaderReady && !activeScan) {
+                        qrReaderReady = false;
+                        let imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
+                        qrReader.postMessage(imageData, [imageData.data.buffer]);
+                    }
+                }
+            } else {
+                loadingMessage.innerText = '⌛ ' + config.language.loading_video;
+            }
+
+            qrReader.onmessage = function(message) {
+                codeInImage = message.data;
+
+                if (codeInImage) {
+                    reservations.search(codeInImage.data).draw();
+
+                    let $scan = $('tr[data-code="'+ codeInImage.data + '"]').find('a[data-action="scan"]');
+
+                    if ($scan.length) {
+                        $scan.trigger('click');
+                    } else {
+                        createModal(
+                            config.language.status.code_not_found.title,
+                            config.language.status.code_not_found.message,
+                            'error'
+                        );
+                    }
+                }
+                qrReaderReady = true;
+            }
+
+            requestAnimationFrame(tick);
+        }
     }
 }
