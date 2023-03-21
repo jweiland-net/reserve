9d7f28559dcac206153da735dd25b9fe9f3d9aab
Stefan Froemken <froemken@gmail.com>
2023-03-21 13:58:44 +0100

    Use Core PageRenderer instead of instantiating a new one

--- Classes/Hooks/PageRenderer.php
+++ Classes/Hooks/PageRenderer.php
@@ -12,29 +12,25 @@ declare(strict_types=1);
 namespace JWeiland\Reserve\Hooks;
 
 use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class PageRenderer
 {
-    const MODAL_SESSION_KEY = 'tx_reserve_modal';
+    public const MODAL_SESSION_KEY = 'tx_reserve_modal';
 
     /**
      * Check the setting tx_reserve_modal (self::MODAL_UC_KEY) and add all necessary data
      * using the information from current BE_USER. Then remove the configuration when the
      * current settings are processed.
-     *
-     * @param array $params
-     * @param \TYPO3\CMS\Core\Page\PageRenderer $pageRenderer
      */
-    public function processTxReserveModalUserSetting(array $params, \TYPO3\CMS\Core\Page\PageRenderer $pageRenderer): void
-    {
+    public function processTxReserveModalUserSetting(
+        array $params,
+        \TYPO3\CMS\Core\Page\PageRenderer $pageRenderer
+    ): void {
         if (
             TYPO3_MODE === 'BE'
             && $this->getBackendUserAuthentication()->user
             && $configuration = $this->getBackendUserAuthentication()->getSessionData(self::MODAL_SESSION_KEY)
         ) {
-            /** @var \TYPO3\CMS\Core\Page\PageRenderer $pageRenderer */
-            $pageRenderer = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Page\PageRenderer::class);
             $pageRenderer->addJsInlineCode(
                 'Require-JS-Module-TYPO3/CMS/Reserve/Backend/AskForMailAfterEditModule',
                 'require(["TYPO3/CMS/Reserve/Backend/AskForMailAfterEditModule"]);'
@@ -43,14 +39,16 @@ class PageRenderer
             foreach ($configuration['jsInlineCode'] as $name => $block) {
                 $pageRenderer->addJsInlineCode($name, $block);
             }
+
             foreach ($configuration['inlineSettings'] as $namespace => $array) {
                 $pageRenderer->addInlineSettingArray($namespace, $array);
             }
+
             foreach ($configuration['inlineLanguageLabel'] as $namespace => $value) {
                 $pageRenderer->addInlineLanguageLabel($namespace, $value);
             }
 
-            // remove modal configuration of current modal
+            // Remove modal configuration of current modal
             $this->getBackendUserAuthentication()->setAndSaveSessionData(self::MODAL_SESSION_KEY, null);
         }
     }
