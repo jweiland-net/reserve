6808a08f6592560631d97cd1eaa1485090250261
Stefan Froemken <froemken@gmail.com>
2023-03-21 11:08:07 +0100

    Implement new php-cs-fixer config

--- Build/php-cs-fixer/config.php
+++ Build/php-cs-fixer/config.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the package jweiland/jobcenter.
+ * This file is part of the package jweiland/reserve.
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
@@ -14,7 +14,7 @@ if (PHP_SAPI !== 'cli') {
 }
 
 $headerComment = <<<COMMENT
-This file is part of the package jweiland/jobcenter.
+This file is part of the package jweiland/reserve.
 
 For the full copyright and license information, please read the
 LICENSE file that was distributed with this source code.
