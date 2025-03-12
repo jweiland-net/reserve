9e1207672a27a55bdb61726d85b8689e66f38160
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-03-12 15:52:58 +0100

    [TASK] Version details updated in ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,10 +15,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'projects@jweiland.net',
     'author_company' => 'jweiland.net',
     'state' => 'stable',
-    'version' => '3.0.1',
+    'version' => '4.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.8-12.4.99',
+            'typo3' => '13.4.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [
